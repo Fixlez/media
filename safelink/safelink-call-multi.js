ab80cd965275bbac2984edf2a7830bf089ecdb2a
No Name <43710608+rejeky2016@users.noreply.github.com>
2018-11-08 01:33:56 +0700

    Update safelink-call-multi.js

--- safelink/safelink-call-multi.js
+++ safelink/safelink-call-multi.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/11/dark-souls-iii-are-you-playing-it.html"}
 if(x=="2"){xxx="/2018/11/the-best-video-games-of-2018.html"}
 if(x=="3"){xxx="/2018/11/top-10-dynamic-seo-strategies-for-2018.html"}
 if(x=="4"){xxx="/2018/11/seo-how-to-make-write-friendly-articles.html"}
-if(x=="5"){xxx="/2018/11/seo-content-guide-to-creating-content.html"}a_to_ve[i].href="https://safelinkalt.blogspot.com/2018/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/11/seo-content-guide-to-creating-content.html"}a_to_ve[i].href="https://safelinkalt.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
